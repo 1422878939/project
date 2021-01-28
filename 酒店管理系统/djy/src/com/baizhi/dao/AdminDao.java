fcf93fbf86ade8f464e34bcdca9fdf49d6a52d36
1422878939 <78014303+1422878939@users.noreply.github.com>
2021-01-28 15:35:28 +0800

    Delete 酒店管理系统/djy directory

--- 酒店管理系统/djy/src/com/baizhi/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.baizhi.dao;
-
-import com.baizhi.entity.Admin;
-import com.baizhi.entity.User;
-
-public interface AdminDao {
-	//通过username的一个查询
-		public Admin queryAdminByUsername(String adminName);
-}
--- 酒店管理系统/djy/src/com/baizhi/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.baizhi.service;
-
-import com.baizhi.entity.Admin;
-
-public interface AdminService {
-	public String adminloginService(String adminName,String adminPass);
-}
